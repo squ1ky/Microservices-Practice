f0649ec9f3eb2659d3f851dac05589111f632199
squ1ky <squ1ky@mail.ru>
2024-05-02 21:19:03 +0300

    first commit

---
+++ customer/src/main/java/com/katharsys/customer/CustomerRegistrationRequest.java
@@ -0,0 +1,7 @@
+package com.katharsys.customer;
+
+public record CustomerRegistrationRequest(
+        String firstName,
+        String lastName,
+        String email) {
+}
---
+++ customer/src/main/java/com/katharsys/customer/CustomerService.java
@@ -0,0 +1,18 @@
+package com.katharsys.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerService {
+    public void registerCustomer(CustomerRegistrationRequest request) {
+        Customer customer = Customer.builder()
+                .firstName(request.firstName())
+                .lastName(request.lastName())
+                .email(request.email())
+                .build();
+
+        //todo : check if email valid
+        //todo : check if email not taken
+        //todo : store customer in db
+    }
+}
